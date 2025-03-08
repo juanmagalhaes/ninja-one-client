3aead72d999e8b68f8dc3a2f1c3df101c1785f26
juanmagalhaes <contato.juanmagalhaes@gmail.com>
2025-03-08 15:39:26 -0300

    Adjust test matcher to only match .ts files

--- jest.config.ts
+++ jest.config.ts
@@ -139,7 +139,7 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["src/**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(test).[tj]s?(x)"],
+  testMatch: ["src/**/__tests__/**/*.ts", "**/?(*.)+(test).ts"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
