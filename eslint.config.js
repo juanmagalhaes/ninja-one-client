7deb0f3ccfeab80d6cab113f2d15954fa8632d0d
juanmagalhaes <contato.juanmagalhaes@gmail.com>
2025-03-08 15:37:58 -0300

    Remove unused import

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,6 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-import jest from "eslint-plugin-jest";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
